6eb1da10d252208dbf217a13e7e5d94627f1fe7e
orcsab <orcsab@gmail.com>
2014-05-28 18:13:45 +0800

    created projects page

--- html/e-scott.js
+++ html/e-scott.js
@@ -1,4 +1,12 @@
 $(function () {
   $('#header').load("html/header.html");
   $('#footer').load("html/footer.html");
+
+  $('#icon-home').hover (function () {
+    console.log('mouseenter');
+    $(this).attr('src', 'html/images/icon-world.png');
+  }, function () {
+    $(this).attr('src', 'html/images/icon-world-inverse.png');
+  });
+
 });
